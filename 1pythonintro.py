f550b6df46b782274ba5c7587516e05d4df3695f
Muhammed Eren Sönmez <erensonmez.bussines@gmail.com>
2024-06-29 19:07:51 +0300

    Add files via upload

---
+++ 1pythonintro.py
@@ -0,0 +1,12 @@
+if 5 > 2:
+    print("Five is greater than two!") 
+# if 5>2
+#print("Five is greater than two!")False
+
+#comments 
+"""
+This is a comment
+written in
+more than just one line
+"""
+
