3bd8d221a68ad2d98a55532676828c121be091b2
Muhammed Eren Sönmez <erensonmez.bussines@gmail.com>
2024-06-29 19:09:38 +0300

    Add files via upload

---
+++ 4pythonstrings.py
@@ -0,0 +1,28 @@
+'''print("Hello")
+print('Hello')''' #They are same things
+
+'''a = "Hello"
+print(a)'''
+
+'''a = "Hello World!"
+print(a[1]) #The first character has the position 0!!!Remember it'''
+
+''''for x in "banana":
+    print(x)'''
+
+'''a = "Hello, World!"
+print(len(a))'''
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt :
+    print("Yes, 'free' is present.")
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt :
+    print("No, 'free' is NOT present.")
